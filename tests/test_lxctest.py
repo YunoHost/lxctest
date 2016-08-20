990798e8a0875f17bd1a716b77c16c533f3c8f15
Joshua Powers <mrpowersj@gmail.com>
2016-08-19 19:52:37 -0600

    removing main from coverage and tests

--- tests/test_lxctest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from lxctest import lxctest
-
-
-class TestLxctest(unittest.TestCase):
-    def TestHappyPath(self):
-        lxctest.main('examples/basic.yaml')
-        assert True
